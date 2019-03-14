59d839a2c8e30eb1b369c4a596b3cf83fd29ac17
Takehiro Ohtani <isit7a@gmail.com>
2019-03-15 07:43:46 +0900

    pagination の実装を修正、他微修正

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const Octokit = require('@octokit/rest')
 
 const optionDefinitions = [
   {name: 'language', alias: 'l', type: String, defaultValue: 'ja', description: 'ja or en are available.'},
-  {name: 'from', alias: 'f', type: String, defaultValue: moment().subtract(6, 'days').format('YYYY-MM-DD'), description: 'get data from this date param.'},
-  {name: 'until', alias: 'u', type: String, defaultValue: moment().add(1, 'days').format('YYYY-MM-DD'), description: 'get data until this date param.'},
+  {name: 'from', alias: 'f', type: String, defaultValue: moment().subtract(6, 'days').format('YYYY-MM-DD'), description: 'get data from this date param, format YYYY-MM-DD.'},
+  {name: 'until', alias: 'u', type: String, defaultValue: moment().format('YYYY-MM-DD'), description: 'get data until this date param, format YYYY-MM-DD.'},
   {name: 'help', alias: 'h', type: Boolean},
 ]
 
@@ -16,6 +16,13 @@ const sections = [
   {
     header: 'Github Activity Reporter',
     content: 'github happy activity reporter 🚀'
+  },
+  {
+    header: 'Synopsis',
+    content: [
+      '$ node index.js <github username> <github personal access token>',
+      '$ node index.js <github username> <github personal access token> -l en --from 2019-01-01 --until 2019-02-01'
+    ]
   },
   {
     header: 'Options',
@@ -26,15 +33,15 @@ const sections = [
 const options = commandLineArgs(optionDefinitions, { partial: true })
 
 if (options.help) {
-  const usage = commandLineUsage(sections);
-  console.log(usage);
-  process.exit(0);
+  const usage = commandLineUsage(sections)
+  console.log(usage)
+  process.exit(0)
 }
 
 if (process.argv.length < 4) {
   console.error('This command need args github account name and personal access token.')
   console.error('example command: npm run get -- {username} {token}')
-  process.exit(1);
+  process.exit(1)
 }
 
 const username = process.argv[2]
@@ -47,27 +54,13 @@ const octokit = new Octokit({
   auth: `token ${token}`
 })
 
-async function getEvents(page = 1, results = []) {
+async function getEvents() {
   const per_page = 100
-  return await octokit.activity.listEventsForUser(
+  const page = 1
+  const options = octokit.activity.listEventsForUser.endpoint.merge(
     {username, per_page, page}
-  ).then(result => {
-    if (!result.data.length) {
-      return results
-    }
-
-    const filtered = filterEvents(result.data)
-    results = results.concat(filtered)
-
-    if (filtered.length !== result.data.length) {
-      return results
-    } else {
-      page++
-      return getEvents(page, results)
-    }
-  }).catch(err => {
-    console.log(err)
-  })
+  )
+  return await octokit.paginate(options)
 }
 
 function filterEvents(items) {
@@ -100,63 +93,75 @@ function getReviewComment(items) {
   return items.filter(item => item.type === 'PullRequestReviewCommentEvent' && item.payload.action === 'created')
 }
 
-getEvents().then(function(items) {
-  const openedIssues = getOpenedIssue(items)
-  const closedIssues = getClosedIssue(items)
-  const openedPrs = getOpenedPr(items)
-  const closedPrs = getClosedPr(items)
-  const issueComments = getIssueComment(items)
-  const reviewComments = getReviewComment(items)
+function main() {
+  getEvents().then(function(items) {
+    items = filterEvents(items)
 
-  // create other context
-  let totalClosedIssueCommentCount = 0
-  for (let item of closedIssues) {
-    totalClosedIssueCommentCount += item.payload.issue.comments
-  }
+    if (!items.length) {
+      console.error('data not found!!')
+      process.exit(1)
+    }
 
-  let totalClosedPrCommitCount = 0
-  let totalClosedPrCommentCount = 0
-  let totalClosedPrReviewCommentCount = 0
-  let totalClosedPrChangedFileCount = 0
-  let totalClosedPrAdditionLineCount = 0
-  let totalClosedPrDeletionLineCount = 0
-  for (let item of closedPrs) {
-    totalClosedPrCommitCount += item.payload.pull_request.commits
-    totalClosedPrCommentCount += item.payload.pull_request.comments
-    totalClosedPrReviewCommentCount += item.payload.pull_request.review_comments
-    totalClosedPrChangedFileCount += item.payload.pull_request.changed_files
-    totalClosedPrAdditionLineCount += item.payload.pull_request.additions
-    totalClosedPrDeletionLineCount += item.payload.pull_request.deletions
-  }
+    const openedIssues = getOpenedIssue(items)
+    const closedIssues = getClosedIssue(items)
+    const openedPrs = getOpenedPr(items)
+    const closedPrs = getClosedPr(items)
+    const issueComments = getIssueComment(items)
+    const reviewComments = getReviewComment(items)
 
-  const otherContext = {
-    totalClosedIssueCommentCount,
-    totalClosedPrCommitCount,
-    totalClosedPrCommentCount,
-    totalClosedPrReviewCommentCount,
-    totalClosedPrChangedFileCount,
-    totalClosedPrAdditionLineCount,
-    totalClosedPrDeletionLineCount
-  }
+    // create other context
+    let totalClosedIssueCommentCount = 0
+    for (let item of closedIssues) {
+      totalClosedIssueCommentCount += item.payload.issue.comments
+    }
 
-  // define context
-  const baseContext = {
-    openedIssues,
-    closedIssues,
-    openedPrs,
-    closedPrs,
-    issueComments,
-    reviewComments
-  }
-  const context = Object.assign(baseContext, otherContext)
+    let totalClosedPrCommitCount = 0
+    let totalClosedPrCommentCount = 0
+    let totalClosedPrReviewCommentCount = 0
+    let totalClosedPrChangedFileCount = 0
+    let totalClosedPrAdditionLineCount = 0
+    let totalClosedPrDeletionLineCount = 0
+    for (let item of closedPrs) {
+      totalClosedPrCommitCount += item.payload.pull_request.commits
+      totalClosedPrCommentCount += item.payload.pull_request.comments
+      totalClosedPrReviewCommentCount += item.payload.pull_request.review_comments
+      totalClosedPrChangedFileCount += item.payload.pull_request.changed_files
+      totalClosedPrAdditionLineCount += item.payload.pull_request.additions
+      totalClosedPrDeletionLineCount += item.payload.pull_request.deletions
+    }
 
-  const result = nunjucks.render(`templates/${options.language}.njk`, context);
+    const otherContext = {
+      totalClosedIssueCommentCount,
+      totalClosedPrCommitCount,
+      totalClosedPrCommentCount,
+      totalClosedPrReviewCommentCount,
+      totalClosedPrChangedFileCount,
+      totalClosedPrAdditionLineCount,
+      totalClosedPrDeletionLineCount
+    }
 
-  console.log('-------------Export markdown start---------------')
-  console.log(result)
-  console.log('-------------Export markdown end---------------')
-  clipboardy.writeSync(result);
-  console.log('Copy complete!! Let\'s paste!!')
-  process.exit(0);
-});
+    // define context
+    const baseContext = {
+      from: options.from,
+      until: options.until,
+      openedIssues,
+      closedIssues,
+      openedPrs,
+      closedPrs,
+      issueComments,
+      reviewComments
+    }
+    const context = Object.assign(baseContext, otherContext)
 
+    const result = nunjucks.render(`templates/${options.language}.njk`, context);
+
+    console.log('-------------Export markdown start---------------')
+    console.log(result)
+    console.log('-------------Export markdown end---------------')
+    clipboardy.writeSync(result);
+    console.log('Copy complete!! Let\'s paste!!')
+    process.exit(0);
+  });
+}
+
+main()
